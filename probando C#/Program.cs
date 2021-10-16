3214fb85b8e35c96fa1630a353b2e63ccd2cbc7a
ArielEzequielPerez <ariperez@frba.utn.edu.ar>
2021-10-16 14:59:16 -0300

    practicando con C#

--- probando C#/Program.cs
+++ probando C#/Program.cs
@@ -1,13 +1,25 @@
 ﻿using System;
 
-namespace probando_C_
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+//Definir una variable numerica, asignarle un valor y sumarle 5.
+int numero = 7; //creo una variable y le asigno n valor.
 
-        }
-    }
-}
+Console.WriteLine($"el numero asignado es: {numero} y si le sumo 5 seria:  {numero + 5}");
+
+
+//Definir dos variables de cadena y asignarles un valor y concatenarlas.
+string primerPalabra = "hola";
+string segundaPalabra = "¿como estas?";
+
+Console.WriteLine($"la primer palabra es: {primerPalabra}");
+Console.WriteLine($"la segunda es: {segundaPalabra}");
+Console.WriteLine($"Si concatenamos la primera y segunda palabra seria: {primerPalabra + " " + segundaPalabra}");
+
+
+
+
+
+
+
+
+
+          
